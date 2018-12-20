204583a80d57ca5a223c6e7c9568ff1305412ab6
JCH <jean-charles.hardy@estaca.eu>
2018-12-20 23:33:21 +0100

    Add tests

--- dfqueue/__init__.py
+++ dfqueue/__init__.py
@@ -7,4 +7,6 @@ from .core.dfqueue import synchronized
 from .core.dfqueue import assign_dataframe
 from .core.dfqueue import list_queue_names
 
+from .core.dfqueue import QueueInfoProvider
+
 from .__meta__ import __version__
