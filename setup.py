0637a4071b5bdc4c58bb92e2cfd7a437fb3e5496
JCH <jean-charles.hardy@estaca.eu>
2018-12-24 01:09:47 +0100

    Add pandas requirement >= 0.23.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(meta_data_file_path) as meta_data_file:
 setup(
     name=package_name,
     version=meta_data['__version__'],
-    install_requires="pandas",
+    install_requires="pandas>=0.23.4",
     packages=find_packages(),
     author=meta_data['__author__'],
     author_email=meta_data['__author_email__'],
